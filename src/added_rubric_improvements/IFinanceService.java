4dffa65ac97a76aee2e6dab203439bf63bdb7aca
Rohit045-a <rohit.24scse1180132@galgotiasuniversity.ac.in>
2025-12-20 11:06:30 +0530

    Delete src directory

--- src/added_rubric_improvements/IFinanceService.java
+++
@@ -1,9 +0,0 @@
-package added_rubric_improvements;
-
-import java.util.List;
-import models.Expense;
-
-public interface IFinanceService {
-    List<Expense> getExpensesForUser(int userId);
-    boolean addExpense(Expense e);
-}
