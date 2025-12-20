f86058067b64aaf1de45c9ec4ed79c79c67b6bd1
Rohit045-a <rohit.24scse1180132@galgotiasuniversity.ac.in>
2025-12-20 12:10:02 +0530

    Delete src directory

--- src/added_rubric_improvements/DataAccessException.java
+++
@@ -1,8 +0,0 @@
-package added_rubric_improvements;
-
-public class DataAccessException extends RuntimeException {
-    public DataAccessException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-    public DataAccessException(String msg) { super(msg); }
-}
